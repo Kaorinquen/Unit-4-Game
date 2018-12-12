82958d5e155b6f0d40e1212aa995df74b639ce9b
unknown <jovan@myradar.com>
2018-12-12 00:42:44 -0500

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4
+
+randomNumber = Math.floor(Math.random() * 120) + 19;
+crystal1 = Math.floor(Math.random() * 12) + 1;
+crystal2 = Math.floor(Math.random() * 12) + 1;
+crystal3 = Math.floor(Math.random() * 12) + 1;
+crystal4 = Math.floor(Math.random() * 12) + 1;
+crystalTotal = 0;
+
