2b761a25b0934d75807e5d3f4eeace3bb56477ea
JovanJSYK <40006077+Kaorinquen@users.noreply.github.com>
2018-12-13 17:04:17 -0500

    for loop changed; functions changed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,24 @@
-var crystal1;
-var crystal2;
-var crystal3;
-var crystal4
-var win = 0;
-var loss = 0;
+// var crystal1;
+// var crystal2;
+// var crystal3;
+// var crystal4
+// var win = 0;
+// var loss = 0;
 
-for (i = 1; i > 5; i++)
-var startOver = function(){
+var startOver = function() {
+for (i = 0; i > 4; i++) {   
 randomNumber = Math.floor(Math.random() * 120) + 19;
-$(".goalNumber").text(randomNumber); 
-crystal1 = Math.floor(Math.random() * 12) + 1;
-crystal2 = Math.floor(Math.random() * 12) + 1;
-crystal3 = Math.floor(Math.random() * 12) + 1;
-crystal4 = Math.floor(Math.random() * 12) + 1;
-crystalTotal = 0;
+$("#crystalNum"[i]) = Math.floor(Math.random() * 12) + 1;
+// crystal2 = Math.floor(Math.random() * 12) + 1;
+// crystal3 = Math.floor(Math.random() * 12) + 1;
+// crystal4 = Math.floor(Math.random() * 12) + 1;
+crystalTotal = 0;  
+console.log("hello world");
+    }
 }
+$("#goalNumber").text(randomNumber)
 
 startOver();
-
 // startOver = function (reset){
 //     crystalTotal = 0;
 //     this.randomNumber;
@@ -28,31 +29,31 @@ var winsLoses = function (){
     if (crystalTotal === randomNumber) {
         $("#rules").html("YOU WIN!" + "<br>" + "Reload the window to star again");
         startOver();
-    }else{ (crystalTotal > randomNumber) {
+    }else if (crystalTotal > randomNumber) { 
     $("#rules").html("You got greedy!" + "<br>" + "Reload the page to start again");
 
 }
 
 $("#crys1").on("click", function() {
-        points = points + parseInt($("#crystal1").attr("crystal1"));
+        points = points + parseInt($("#crystalNum1").attr("crystal1"));
         $(".points").text(points);
         winLose();
     },
 
     $("#crys2").on("click", function() {
-        points = points + parseInt($("#crystal2").attr("crystal2"));
+        points = points + parseInt($("#crystalNum2").attr("crystal2"));
         $(".points").text(points);
         winLose();
     },
 
     $("#crys3").on("click", function() {
-        points = points + parseInt($("#crystal3").attr("crystal3"));
+        points = points + parseInt($("#crystalNum3").attr("crystal3"));
         $(".points").text(points);
         winLose();
     },
 
     $("#crys4").on("click", function() {
-        points = points + parseInt($("#crystal4").attr("crystal4"));
+        points = points + parseInt($("#crystalNum4").attr("crystal4"));
         $(".points").text(points);
         winLose();
-    });
+    },
