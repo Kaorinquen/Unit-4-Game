219a54cc471ca3a31dea2fc57d6ebd6f3ac74b74
Kaorinquen <jovan@myradar.com>
2018-12-13 00:35:15 -0500

    functions and listeners added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ var crystal1;
 var crystal2;
 var crystal3;
 var crystal4
+var win = 0;
+var loss = 0;
 
 randomNumber = Math.floor(Math.random() * 120) + 19;
 crystal1 = Math.floor(Math.random() * 12) + 1;
@@ -10,3 +12,10 @@ crystal3 = Math.floor(Math.random() * 12) + 1;
 crystal4 = Math.floor(Math.random() * 12) + 1;
 crystalTotal = 0;
 
+var reset = function (reset){
+    crystalTotal = 0;
+    this.randomNumber;
+    console.log(reset);
+}
+
+crystal1.randomNumber
